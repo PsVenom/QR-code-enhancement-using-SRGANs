20fe2cc70123f85151b11138efdce5523d7d4a55
PsVenom <99831413+PsVenom@users.noreply.github.com>
2022-04-04 13:25:59 +0530

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -5,16 +5,6 @@ def build_data(data):
   cropped=tf.dtypes.cast(tf.image.random_crop(data['image'] / 255,(128,128,3)),tf.float32)
   lr=tf.image.resize(cropped,(32,32))
   return (lr,cropped * 2 - 1)
-train_dataset_mapped = train_data.map(build_data,num_parallel_calls=tf.data.AUTOTUNE)
-for x in train_dataset_mapped.take(1):
-  plt.imshow(x[0].numpy())
-  plt.show()
-  plt.imshow(bicubic_interpolate(x[0].numpy(),(128,128)))
-  plt.show()
-  plt.imshow(x[1].numpy())
-  plt.show()
-  
-  
   
   #defining the resnet
 def residual_block_gen(ch=64,k_s=3,st=1):

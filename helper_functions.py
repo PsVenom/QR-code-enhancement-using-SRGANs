da76bbcf341b811531eeb29baee341ace22a0ca1
PsVenom <99831413+PsVenom@users.noreply.github.com>
2022-04-04 12:43:15 +0530

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -30,17 +30,11 @@ def Upsample_block(x, ch=256, k_s=3, st=1):
   x = tf.nn.depth_to_space(x, 2) # Subpixel pixelshuffler
   x = tf.keras.layers.LeakyReLU()(x)
  return x
-input_lr=tf.keras.layers.Input(shape=(None,None,3))
-input_conv=tf.keras.layers.Conv2D(64,9,padding='same')(input_lr)
-input_conv=tf.keras.layers.LeakyReLU()(input_conv)
-SRRes=input_conv
-for x in range(5):
-  res_output=residual_block_gen()(SRRes)
-  SRRes=tf.keras.layers.Add()([SRRes,res_output])
-SRRes=tf.keras.layers.Conv2D(64,9,padding='same')(SRRes)
-SRRes=tf.keras.layers.BatchNormalization()(SRRes)
-SRRes=tf.keras.layers.Add()([SRRes,input_conv])
-SRRes=Upsample_block(SRRes)
-SRRes=Upsample_block(SRRes)
-output_sr=tf.keras.layers.Conv2D(3,9,activation='tanh',padding='same')(SRRes)
-SRResnet=tf.keras.models.Model(input_lr,output_sr)
+
+def residual_block_disc(ch=64,k_s=3,st=1):
+  model=tf.keras.Sequential([
+  tf.keras.layers.Conv2D(ch,k_s,strides=(st,st),padding='same'),
+  tf.keras.layers.BatchNormalization(),
+  tf.keras.layers.LeakyReLU(),
+  ])
+  return model

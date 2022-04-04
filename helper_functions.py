6bb5df59d0602570d24b987783eb3de681d43ddc
PsVenom <99831413+PsVenom@users.noreply.github.com>
2022-04-04 13:45:31 +0530

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -30,3 +30,11 @@ def residual_block_disc(ch=64,k_s=3,st=1):
   tf.keras.layers.LeakyReLU(),
   ])
   return model
+cross_entropy = tf.keras.losses.BinaryCrossentropy()
+def discriminator_loss(real_output, fake_output):
+  real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+  fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+  total_loss = real_loss + fake_loss
+  return total_loss
+def generator_loss(fake_output):
+  return cross_entropy(tf.ones_like(fake_output), fake_output)
